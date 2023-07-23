3eeedfc95388bf16de81931d01d6bb78419430ab
Hanbit Kang <hanbitkang95@gmail.com>
2023-07-23 15:39:23 +0900

    feat: Add new components for sidebar, detail content and main feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Main from './components/Main/Main';
 
 const queryClient = new QueryClient();
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalStyles />
+      <Main />
     </QueryClientProvider>
   );
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SideBar from '../SideBar/SideBar';
+import DetailContent from '../DetailContent/DetailContent';
+
+function Main() {
+  return (
+    <div>
+      <SideBar />
+      <DetailContent />
+    </div>
+  );
+}
+
+export default Main;
