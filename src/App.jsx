b6f8abf3e193dd66a30516ae5df0e6b42e20b256
Hanbit Kang <hanbitkang95@gmail.com>
2023-07-23 15:12:57 +0900

    docs: react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { GlobalStyles } from './styles/GlobalStyles';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
-      <h1>Hello</h1>
-    </>
+    </QueryClientProvider>
   );
 }
 
