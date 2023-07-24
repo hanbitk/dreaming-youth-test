a1a475f44553879d2d82de4ffe7bbdfcb1a5d9af
Hanbit Kang <hanbitkang95@gmail.com>
2023-07-24 13:17:13 +0900

    feat: detail content structure

--- src/components/DetailContent/DetailContent.jsx
+++ src/components/DetailContent/DetailContent.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import {
+  StDetailContainer,
+  StDetailForm,
+  StDetailFormBox,
+  StDetailFormButton,
+  StDetailHeader
+} from '../../styles/DetailContent.styles';
 
 function DetailContent() {
-  return <div>DetailContent</div>;
+  return (
+    <StDetailContainer>
+      <StDetailHeader>
+        <h2>타이틀</h2>
+      </StDetailHeader>
+      <StDetailForm>
+        <StDetailFormBox>
+          <div>
+            <label>정보 1</label>
+            <p>정보 내용</p>
+          </div>
+        </StDetailFormBox>
+        <StDetailFormButton>저장</StDetailFormButton>
+      </StDetailForm>
+    </StDetailContainer>
+  );
 }
 
 export default DetailContent;
--- src/components/DetailContent/DetailContent2.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function DetailContent2() {
-  return <div>DetailContent2</div>;
-}
-
-export default DetailContent2;
